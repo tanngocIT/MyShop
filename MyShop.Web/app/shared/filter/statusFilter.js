bddc9421bd75342e298002449ab47d0fc681c08b
TanNgoc <tanngocIT@gmail.com>
2016-07-13 13:00:01 +0700

    Lesson 24

---
+++ MyShop.Web/app/shared/filter/statusFilter.js
@@ -0,0 +1,10 @@
+﻿(function (app) {
+    app.filter('statusFilter', function () {
+        return function (input) {
+            if (input)
+                return 'Kích hoạt';
+            else
+                return 'Khóa';
+        }
+    });
+})(angular.module('myshop.common'));
